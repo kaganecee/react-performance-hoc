abb229a1efb7d8e79e2811520610a5c254048a44
Bilge Kağan Ece <kaganecee@gmail.com>
2022-08-01 01:43:19 +0300

    params updated

--- withPerformance.js
+++ withPerformance.js
@@ -1,25 +1,20 @@
-import { useEffect } from "react";
-import React from "react"
-const withPerformance = (Component,errorTime=200,warnTime=100) => ({...props}) => {
-    let startTime;
-    let endTime;
-    startTime = performance.now();
-    useEffect(()=> {
-        <Component/>
-        endTime = performance.now();
-        if(endTime-startTime > errorTime){
-            console.error(Component.name,"render time =",endTime - startTime)
+import React from "react";
+const withPerformance = (Component,identifier="specify identifier",errorTime=200,warnTime=100,onPerformanceCapture=defaultOnPerformanceCapture) => React.forwardRef(( props,ref ) => {
+    const startTime = performance.now();
+    const renderedComponent = <Component {...props} ref={ref}/>;
+    const endTime = performance.now();
+    const renderedTime = endTime - startTime
+    onPerformanceCapture(Component,identifier,renderedTime,errorTime,warnTime)
+    return renderedComponent
+})
+const defaultOnPerformanceCapture = (Component,identifer,renderedTime,errorTime=200,warnTime=100) => {
+        if(renderedTime > errorTime){
+            console.error(Component.name,identifer,"render time =",renderedTime)
         }
-        else if(endTime-startTime > warnTime){
-            console.warn(Component.name,"render time =",endTime-startTime)
+        else if(renderedTime > warnTime){
+            console.warn(Component.name,identifer,"render time =",renderedTime)
         }else{
-            console.log(Component.name,"render time =",endTime - startTime)
+            console.log(Component.name, identifer,"render time =",renderedTime)
         }
-      })
-    return (
-        <>
-            <Component  {...props}/>
-        </>
-    )
-}
+    }
 export default withPerformance;
